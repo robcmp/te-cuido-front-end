2d5f51e64c7e6a4392959b1df7625d4f398c7443
muscatelli <muscatelli92@gmail.com>
2021-10-06 19:01:44 -0500

    making-login

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import { withFormik, Form, Field } from 'formik'
+import React from 'react';
+import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
 const Login = (props) => {
   const loginStyle = {
@@ -9,8 +10,8 @@ const Login = (props) => {
     padding: "30px",
     borderRadius: "10px",
     boxShadow: "0px 0px 10px 10px rgba(0,0,0,0.15)"
-  }
-
+  };
+  const { touched, errors } = props;
   return(
     <React.Fragment>
       <div className="container">
@@ -20,17 +21,19 @@ const Login = (props) => {
             <div className="form-group">
               <label htmlFor="email">Email</label>
               <Field type="text" name="email" className={"form-control"} placeholder="Email" />
+              { touched.email && errors.email && <span className="help-block text-danger">{errors.email}</span> }
             </div>
             <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <Field type="password" name="password" className={"form-control"} placeholder="Password" />
+              <label htmlFor="password">Password</label>
+              <Field type="password" name="password" className={"form-control"} placeholder="Password" />
+              { touched.password && errors.password && <span className="help-block text-danger">{errors.password}</span> }
             </div>
             <button type="submit" className="btn btn-primary">Login</button>
           </Form>
         </div>
       </div>
     </React.Fragment>
-  )
+  );
 }
 
 const LoginFormik = withFormik({
@@ -40,9 +43,30 @@ const LoginFormik = withFormik({
       password: props.password || ''
     }
   },
+  validationSchema: Yup.object().shape({
+    email: Yup.string().email('Email not valid').required('Email is required'),
+    password: Yup.string().required('Password is required')
+  }),
   handleSubmit: (values) => {
-    console.log(values)
+    const REST_API_URL = "YOUR_REST_API_URL";
+    fetch(REST_API_URL, {
+      method: 'post',
+      body: JSON.stringify(values)
+    }).then(response=> {
+      if (response.ok) {
+        return response.json();
+      } else {
+        // HANDLE ERROR
+        throw new Error('Something went wrong');
+      }
+    }).then(data => {
+      // HANDLE RESPONSE DATA
+      console.log(data);
+    }).catch((error) => {
+      // HANDLE ERROR
+      console.log(error);
+    });
   }
-})(Login)
+})(Login);
 
 export default LoginFormik
