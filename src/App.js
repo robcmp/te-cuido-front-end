5f22b9cae835c83343fa762884f024e654c2d6f3
muscatelli <muscatelli92@gmail.com>
2021-10-07 20:32:17 -0500

    add app.js, layouts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
-import React from 'react';
-import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
-import Login from './views/Login';
+import "./App.css";
+import Layout from "./components/Layout";
 
 function App() {
-  return (
-    <Router>
-      <navbar/>
-      <div className= "App">
-
-        <swith>
-          <Route>
-            <Login />
-          </Route>
-
-        </swith>
-
-
-
-      </div>
-    </Router>
-  );
+  return <Layout />;
 }
 export default App;
