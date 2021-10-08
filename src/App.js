2660aaa90cb3e9ec3d1991425d063c3f0ce98059
muscatelli <muscatelli92@gmail.com>
2021-10-07 19:21:17 -0500

    trying some git things

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -23,13 +22,6 @@ function App() {
       </div>
     </Router>
   );
-=======
-import "./App.css";
-import Layout from "./components/Layout";
-
-function App() {
-  return <Layout />;
->>>>>>> 0214123d4226cf24177ead6f670a26e4ae743051
 }
 
 export default App;
