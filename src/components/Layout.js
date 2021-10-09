aa78872e40fbe908a54aaddf8a50fb83ea673639
arielmarvin@gmail.com <arielmarvin@gmail.com>
2021-10-08 22:33:30 -0400

    cambioariel

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,6 +3,7 @@ import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import Home from "../views/Home";
 import Navbar from "./Navbar";
 import Login from "../views/Login"
+import Register from "./RegisterForm";
 
 const Layout = () => {
   return (
@@ -15,6 +16,9 @@ const Layout = () => {
           </Route>
           <Route exact path="/Login">
             <Login />
+          </Route> 
+          <Route exact path="/Register">
+            <Register />
           </Route> 
           <Route render={() => <h1>Not found</h1>}></Route>
         </Switch>
--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,46 +1,84 @@
 import React from "react";
-
+import { Formik } from "formik";
+import * as yup from "yup";
 const RegisterForm=()=>{
   return(
     <>
-      <form className="form">
-        <div>
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name" placeholder="example-name=Andres"/>
-        </div>
-        <div>
-          <label htmlFor="lastname">LastName</label>
-          <input type="text" id="lastname" name="lastname" placeholder="example-lastname=Bravo"/>
-        </div>
-        <div>
-          <label htmlFor="username">UserName</label>
-          <input type="text" id="username" name="username" placeholder="example-user=Andres-Bravo"/>
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input type="text" id="password" name="password" placeholder="example-password=123aaA/*"/>
-        </div>
-        <div>
-          <label htmlFor="repeat-password">RepeatPassword</label>
-          <input type="text" id="repeat-password" name="repeat-password" placeholder="repeat-password=123aaA/*"/>
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input type="text" id="email" name="email" placeholder="example-email=andres.bravo@gmail.com"/>
-        </div>
-        <div>
-          <label htmlFor="id">ID</label>
-          <input type="text" id="ID" name="id" placeholder="example-id=514232261a"/>
-        </div>
-
-
-      </form>
-    
-    </>
- 
+      const schema = yup.object().shape({
+    firstName:yup.string().required(),
+    lastname:yup.string().required(),
+    username:yup.string().required(),
+   
+  });
+  return(
+  <div className ="RegisterUser">
+    <Formik
+     ValidationSchema={schema}
+     onSubmit={console.log}
+     initialValues={{
+       firstName:"",
+       lastName:"",
+       userName:""
+     }}>
+     {({handleSubmit,handleChange,values,touched,errors})=>
+    <Formik noValidate onSubmit = {handleSubmit}>
+      <Form.Group md = "" controlId="validationFormik01">
+        <Form.Label>First Name</Form.Label>
+        <Form.Control
+        type="text"
+        name="firstName"
+        placeholder ="First Name"
+        value={values.firstName}
+        onChange={handleChange}
+        isValid={touched.firstName && !errors.firstName}
+        isInvalid ={!errors.lastName}
+        />
+        <Form.Control.Feedback type ="invalid">
+          {errors.firstName}
+        </Form.Control.Feedback>
+      </Form.Group>
+      <Form.Group controlId = "validationFormik02">
+        <Form.Label>LastName</Form.Label>
+        <Form.Control
+        type="text"
+        name="firstName"
+        placeholder ="First Name"
+        value={values.firstName}
+        onChange={handleChange}
+        isValid={touched.firstName && !errors.firstName}
+        isInvalid ={!!errors.lastName}
+        isValid={touched.lastName && !errors.lastName}
+        />
+           <Form.Control.Feedback type= "invalid">
+              {errors.lastName}
+           </Form.Control.Feedback>
+   </Form.Group>
+   <Form.Group as={Col} controlId= "validationFormik03">
+     <Form.label>UserName</Form.label>
+     <InputGroup hasValidation>
+        <InputGroup.Prepend>
+          <InputGroup.Text id ="inputGroupPrepend">@</InputGroup.Text>
+        </InputGroup.Prepend>
+        <Form.Control
+          type="text"
+          placeholder="UserName"
+          aria-describedby="inputGroupPrepend"
+          name="username"
+          value={values.username}
+          onChange={handleChange}
+          isInvalid={!!errors.username}
+          isValid={touched.username && !errors.username}
+        />
+        <Form.Control.Feedback type="invalid">
+            {errors.username}
+        </Form.Control.Feedback>
+    </InputGroup>
+   </Form.Group>
+    </Formik>}
+  </div>
+     );
      );
  }
 
-  export default RegisterForm;
-  
+  export default RegisterForm
 
