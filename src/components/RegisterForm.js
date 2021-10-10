faa736b8851ac7c89f8e59419dad1367fe5bbefb
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-10-10 13:02:15 -0300

    deleted anonymous html element, added class to each div of the form

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -31,136 +31,120 @@ const RegisterForm = () => {
   });
   //aqui empieza la estructura
   return (
-    <>
-      <Formik
-        initialValues={{
-          Name: "",
-          LastName: "",
-          UserName: "",
-          Password: "",
-          confirmPassword: "",
-          Email: "",
-          MobilePhone: "",
-        }}
-        validationSchema={formSchema}
-        onSubmit={(values) => console.log(values)}
-      >
-        <Form>
-          <div className="container">
-            <div className="row">
-              <div className="col">
-                <div>
-                  <label htmlFor="Name">Nombres:</label>
-                  <Field
-                    className="form-control"
-                    name="Name"
-                    placeholder=""
-                    type="text"
-                  />
-                  <ErrorMessage
-                    name="Name"
-                    component="div"
-                    className="field-error text-danger"
-                  />
-                </div>
-
-                <div>
-                  <label htmlFor="LastName">Apellidos:</label>
-                  <Field
-                    className="form-control"
-                    name="LastName"
-                    placeholder=""
-                    type="text"
-                  />
-                  <ErrorMessage
-                    name="LastName"
-                    component="div"
-                    className="field-error text-danger"
-                  />
-                </div>
-
-                <div>
-                  <label htmlFor="UserName">Usuario:</label>
-                  <Field
-                    className="form-control"
-                    name="UserName"
-                    placeholder=""
-                    type="text"
-                  />
-                  <ErrorMessage
-                    name="UserName"
-                    component="div"
-                    className="field-error text-danger"
-                  />
-                </div>
-                <div>
-                  <label htmlFor="Email">Correo:</label>
-                  <Field
-                    className="form-control"
-                    name="Email"
-                    placeholder=""
-                    type="email"
-                  />
-                  <ErrorMessage
-                    name="Email"
-                    component="div"
-                    className="field-error text-danger"
-                  />
-                </div>
-                <div>
-                  <label htmlFor="Password">Contraseña:</label>
-                  <Field
-                    className="form-control"
-                    name="Password"
-                    placeholder=""
-                    type="password"
-                  />
-                  <ErrorMessage
-                    name="Password"
-                    component="div"
-                    className="field-error text-danger"
-                  />
-                </div>
-                <div>
-                  <label htmlFor="confirmPassword">Repetir contraseña:</label>
-                  <Field
-                    className="form-control"
-                    name="confirmPassword"
-                    placeholder=""
-                    type="password"
-                  />
-                  <ErrorMessage
-                    name="confirmPassword"
-                    component="div"
-                    className="field-error text-danger"
-                  />
-                </div>
-                <div>
-                  <label htmlFor="ID">RUT:</label>
-                  <Field
-                    className="form-control"
-                    name="ID"
-                    placeholder=""
-                    type="string"
-                  />
-                  <ErrorMessage
-                    name="ID"
-                    component="div"
-                    className="field-error text-danger"
-                  />
-                </div>
+    <Formik
+      initialValues={{
+        Name: "",
+        LastName: "",
+        UserName: "",
+        Password: "",
+        confirmPassword: "",
+        Email: "",
+        MobilePhone: "",
+      }}
+      validationSchema={formSchema}
+      onSubmit={(values) => console.log(values)}
+    >
+      <Form>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <div>
+                <label htmlFor="Name">Nombres:</label>
+                <Field
+                  className="form-control"
+                  name="Name"
+                  placeholder=""
+                  type="text"
+                />
+                <ErrorMessage
+                  name="Name"
+                  component="div"
+                  className="field-error text-danger"
+                />
               </div>
-              <div className="col">
-                <img
-                  src="https://images.unsplash.com/photo-1422015347944-9dd46d16bd0b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGVsZGVybHl8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-                  alt=""
+
+              <div className="form-group">
+                <label htmlFor="LastName">Apellidos:</label>
+                <Field
+                  className="form-control"
+                  name="LastName"
+                  placeholder=""
+                  type="text"
+                />
+                <ErrorMessage
+                  name="LastName"
+                  component="div"
+                  className="field-error text-danger"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="Email">Correo:</label>
+                <Field
+                  className="form-control"
+                  name="Email"
+                  placeholder=""
+                  type="email"
+                />
+                <ErrorMessage
+                  name="Email"
+                  component="div"
+                  className="field-error text-danger"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="Password">Contraseña:</label>
+                <Field
+                  className="form-control"
+                  name="Password"
+                  placeholder=""
+                  type="password"
+                />
+                <ErrorMessage
+                  name="Password"
+                  component="div"
+                  className="field-error text-danger"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="confirmPassword">Repetir contraseña:</label>
+                <Field
+                  className="form-control"
+                  name="confirmPassword"
+                  placeholder=""
+                  type="password"
+                />
+                <ErrorMessage
+                  name="confirmPassword"
+                  component="div"
+                  className="field-error text-danger"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="ID">RUT:</label>
+                <Field
+                  className="form-control"
+                  name="ID"
+                  placeholder=""
+                  type="string"
+                />
+                <ErrorMessage
+                  name="ID"
+                  component="div"
+                  className="field-error text-danger"
                 />
               </div>
             </div>
+            <div className="col-md-4">
+              <img
+                className="img-fluid"
+                src="https://images.unsplash.com/photo-1422015347944-9dd46d16bd0b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGVsZGVybHl8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                alt=""
+              />
+            </div>
           </div>
-        </Form>
-      </Formik>
-    </>
+        </div>
+      </Form>
+    </Formik>
   );
 };
 
