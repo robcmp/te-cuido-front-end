90879298f43f53434ad4864ee44506203229f884
Ruben Colmenares <ruben.colmenares@iesa.edu.ve>
2021-10-10 18:50:29 -0300

    se creó ruta para vista del usuario, se modificó el Layout para eso. Se dejó comentado en el Login el history y useHistory que se van a usar para enlazar la respuesta Ok de la API con la vista del usuario

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,8 +2,9 @@ import React from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import Home from "../views/Home";
 import Navbar from "./Navbar";
-import Login from "../views/Login"
+import Login from "../views/Login";
 import Register from "./RegisterForm";
+import User from "../views/User";
 
 const Layout = () => {
   return (
@@ -16,10 +17,13 @@ const Layout = () => {
           </Route>
           <Route exact path="/Login">
             <Login />
-          </Route> 
+          </Route>
+          <Route exact path="/User">
+            <User />
+          </Route>
           <Route exact path="/Register">
             <Register />
-          </Route> 
+          </Route>
           <Route render={() => <h1>Not found</h1>}></Route>
         </Switch>
       </div>
