c53786c7ee33fd86fe196ac9378cb694ee194513
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-10-20 19:15:09 -0300

    modifed imported style file name

--- src/components/SidebarAdmin.js
+++ src/components/SidebarAdmin.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { slide as Menu } from "react-burger-menu";
-import "../styles/sidebartest.css";
+import "../styles/sidebarAdmin.css";
 
 const SidebarAdmin = (props) => {
   // const clickToggle = () => {
