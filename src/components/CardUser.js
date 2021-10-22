164d02384e855b200aced8ecaab57c8f55df57a7
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-10-21 22:31:42 -0300

    modified div with row to d-flex and div with cols to d-flex to handle better position

--- src/components/CardUser.js
+++ src/components/CardUser.js
@@ -3,23 +3,22 @@ import "../index.css";
 import { Link } from "react-router-dom";
 import "../styles/card.css";
 
-
 const CardUser = (props) => {
   return (
     <div className="card scrolling-wrapper borders">
       <img src={props.image} className="card-img-top" alt="" />
       <div className="card-body">
-       <h5 className="card-title text-center">{props.data.name}</h5>
+        <h5 className="card-title text-center">{props.data.name}</h5>
       </div>
       <div className="card-footer text-center">
-        <div className="row">
-          <div className="col-md-4 ">
+        <div className="d-flex justify-content-evenly">
+          <div className="d-flex">
             <button className="btn btn-primary"> BAN </button>
           </div>
-          <div className="col-md-4">
+          <div className="d-flex">
             <button className="btn btn-primary"> DELETE </button>
           </div>
-          <div className="col-md-4">
+          <div className="d-flex">
             <button className="btn btn-primary"> EDIT </button>
           </div>
         </div>
