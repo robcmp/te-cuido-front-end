026d231a09cf4e5a38ec5a7c6398c1514c590f0c
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-10-21 23:45:40 -0300

    added modal to button edit, added alert to button ban, modified card with d-flex, addded function banConfirmation to handle alert

--- src/components/CardUser.js
+++ src/components/CardUser.js
@@ -2,28 +2,96 @@ import React from "react";
 import "../index.css";
 import { Link } from "react-router-dom";
 import "../styles/card.css";
+import Swal from "sweetalert2";
 
 const CardUser = (props) => {
+  const banConfirmation = () => {
+    Swal.fire({
+      title: "Do you want to save the changes?",
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: "Save",
+      denyButtonText: `Don't save`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire("Saved!", "", "success");
+      } else if (result.isDenied) {
+        Swal.fire("Changes are not saved", "", "info");
+      }
+    });
+  };
   return (
-    <div className="card scrolling-wrapper borders">
-      <img src={props.image} className="card-img-top" alt="" />
-      <div className="card-body">
-        <h5 className="card-title text-center">{props.data.name}</h5>
-      </div>
-      <div className="card-footer text-center">
-        <div className="d-flex justify-content-evenly">
-          <div className="d-flex">
-            <button className="btn btn-primary"> BAN </button>
-          </div>
-          <div className="d-flex">
-            <button className="btn btn-primary"> DELETE </button>
-          </div>
-          <div className="d-flex">
-            <button className="btn btn-primary"> EDIT </button>
+    <>
+      <div className="card scrolling-wrapper borders">
+        <img src={props.image} className="card-img-top" alt="" />
+        <div className="card-body">
+          <h5 className="card-title text-center">{props.data.name}</h5>
+        </div>
+        <div className="card-footer text-center">
+          <div className="d-flex justify-content-evenly">
+            <div className="d-flex">
+              <button className="btn btn-primary" onClick={banConfirmation}>
+                {" "}
+                BAN{" "}
+              </button>
+            </div>
+            <div className="d-flex">
+              <button className="btn btn-primary"> DELETE </button>
+            </div>
+            <div className="d-flex">
+              <button
+                className="btn btn-primary"
+                data-bs-toggle="modal"
+                data-bs-target="#staticBackdrop"
+              >
+                {" "}
+                EDIT{" "}
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+
+      <div
+        className="modal fade"
+        id="staticBackdrop"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabindex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="staticBackdropLabel">
+                Modal title
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">...</div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" className="btn btn-primary">
+                Understood
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
