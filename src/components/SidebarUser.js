8d9b9e0f60c2803853704777fd0ef91fee096bee
carlos martinez <muscatelli92@gmail.com>
2021-10-21 21:45:27 -0500

    sidebar user icons

--- src/components/SidebarUser.js
+++ src/components/SidebarUser.js
@@ -6,10 +6,10 @@ const SidebarUser = () => {
   return (
     <Menu>
       <a className="menu-item" href="/">
-        <i class="bi bi-person-square"></i> Lista de Usuarios
+        <i class="bi bi-person-square"></i> Mi Perfil
       </a>
       <a className="menu-item" href="/salads">
-        <i class="bi bi-cart-dash"></i> Test
+        <i class="bi bi-list-task"></i> Mis Servicios
       </a>
     </Menu>
   );
