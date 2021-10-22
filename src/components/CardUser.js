d99f05942693818b6bdf68c653d595507a8656cc
carlos martinez <muscatelli92@gmail.com>
2021-10-21 19:55:15 -0500

    styles for admin and user

--- src/components/CardUser.js
+++ src/components/CardUser.js
@@ -1,22 +1,26 @@
 import React from "react";
+import "../index.css";
 import { Link } from "react-router-dom";
+import "../styles/card.css";
+
 
 const CardUser = (props) => {
   return (
-    <div className="card">
+    <div className="card scrolling-wrapper borders">
       <img src={props.image} className="card-img-top" alt="" />
       <div className="card-body">
-        <h5 className="card-title">{props.data.name}</h5>
+       <h5 className="card-title text-center">{props.data.name}</h5>
       </div>
-      <div className="card-footer text-center ">
+      <div className="card-footer text-center">
         <div className="row">
-          <div className="col-md-8">
-            <button className="btn btn-primary">Learn more!</button>
+          <div className="col-md-4 ">
+            <button className="btn btn-primary"> BAN </button>
           </div>
           <div className="col-md-4">
-            <button className="btn btn-outline-warning">
-              <i className="bi bi-heart"></i>
-            </button>
+            <button className="btn btn-primary"> DELETE </button>
+          </div>
+          <div className="col-md-4">
+            <button className="btn btn-primary"> EDIT </button>
           </div>
         </div>
       </div>
