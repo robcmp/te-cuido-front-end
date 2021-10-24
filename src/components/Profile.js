2650e1feee15984fdf79e018f2734a60ccd36d78
Ruben Colmenares <ruben.colmenares@iesa.edu.ve>
2021-10-24 13:51:09 -0300

    agregada ruta user/profile con formulario

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,19 +1,21 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Context } from '../store/appContext';
-import { Link, useParams} from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import SidebarUser from "../components/SidebarUser";
 
 
 const Profile = () => {
-
-    const { store, actions } = useContext(Context);
-    const params = useParams();
-
-    useEffect(() => {
-        actions.getPeopleinfo(params.id)
-    }, [])
+    const welcome = JSON.parse(localStorage.getItem("loginUser"));
+    console.log(welcome);
 
     return (
+        <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-4">
+          <SidebarUser />
+         
+        </div>
+        <div className="col-md-8"></div>
+      </div>
         <div className="container-fluid d-flex justify-content-center mt-5">
             <div className="card" style={{ width: "630px", height: "auto" }}>
 
@@ -23,27 +25,28 @@ const Profile = () => {
                     </div>
                     <div className="col-md-4">
                         <div className="card-body">
-                            <h5 className="card-title">{store.profileUser.name}</h5>
+                            <h5 className="card-title">{welcome.user.name}</h5>
                             <p className="card-text">
-                                <p>Apellido: {store.profileUser.last_name}</p>
-                                <p>Contraseña: {store.profileUser.password}</p>
-                                <p>Email: {store.profileUser.email}</p>
-                                <p>ID: {store.profileUser.number_id}</p>
-                                <p>País: {store.profileUser.country}</p>
-                                <p>Ciudad: {store.profileUser.city}</p>
-                                <p>Teléfono: {store.profileUser.phone}</p>
-                                <p>Ocupación: {store.profileUser.occupation}</p>
+                                <p>Apellido: {welcome.user.last_name}</p>
+                                <p>Contraseña: {welcome.user.password}</p>
+                                <p>Email: {welcome.user.email}</p>
+                                <p>ID: {welcome.user.number_id}</p>
+                                <p>País: {welcome.user.country}</p>
+                                <p>Ciudad: {welcome.user.city}</p>
+                                <p>Teléfono: {welcome.user.phone}</p>
+                                <p>Ocupación: {welcome.user.occupation}</p>
 
                             </p>
                             <div className="d-flex justify-content-between">
-                        <Link to={"/"} className="btn btn-outline-primary">Back</Link>
-                    </div>
+                                <Link to={"/"} className="btn btn-outline-primary">Back</Link>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        );
-  };
-  
-  export default Profile;
+        </div>
+    );
+};
+
+export default Profile;
