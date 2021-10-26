88f3a2bb53440ca0951f3dfb11e6bc464e011b15
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-10-26 07:57:37 -0300

    updated with minor changes

--- src/views/Register.js
+++ src/views/Register.js
@@ -1,7 +1,7 @@
 import React from "react";
-import RegisterForm from "../components/RegisterForm"
+import RegisterForm from "../components/RegisterForm";
 const Register = () => {
-  return <RegisterForm/>;
+  return <RegisterForm />;
 };
 
 export default Register;
