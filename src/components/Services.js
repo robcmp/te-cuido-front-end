d34c9fc228d12f88d5e705a701621e6a133eaa1c
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-10-27 08:54:47 -0300

    reworked html markup, reorganization of the bootstrap grid system, modification in text translated to Spanish(need grammar corrections)

--- src/components/Services.js
+++ src/components/Services.js
@@ -21,78 +21,85 @@ const Services = () => {
         }}
       >
         {({ values }) => (
-          <Form>
-            {/* 
-            This first checkbox will result in a boolean value being stored. Note that the `value` prop
-            on the <Field/> is omitted
-          */}
+          <div className="mt-5 pt-5 w-50 mx-auto">
+            <div class="card border-info">
+              {/* <img src="..." class="card-img-top" alt="..." /> */}
+              <h5 class="card-header">Publicacion de Servicio</h5>
+              <div class="card-body">
+                <Form>
+                  <div className="row">
+                    <div role="group" aria-labelledby="checkbox-group">
+                      <label>
+                        <Field type="checkbox" name="checked" value="Hombre" />
+                        Hombre
+                      </label>
+                      <label>
+                        <Field type="checkbox" name="checked" value="Mujer" />
+                        Mujer
+                      </label>
+                    </div>
 
-            {/* 
-            Multiple checkboxes with the same name attribute, but different
-            value attributes will be considered a "checkbox group". Formik will automagically
-            bind the checked values to a single array for your benefit. All the add and remove
-            logic will be taken care of for you.
-          */}
-            <div role="group" aria-labelledby="checkbox-group">
-              <label>
-                <Field type="checkbox" name="checked" value="Hombre" />
-                Hombre
-              </label>
-              <label>
-                <Field type="checkbox" name="checked" value="Mujer" />
-                Mujer
-              </label>
-            </div>
-            <div className="row">
-              <div className="col-md-4">
-                <label htmlFor="edad minima">Edad minima:</label>
-                <Field
-                  className="form-control"
-                  name="edad-min"
-                  placeholder="min"
-                  type="int"
-                />
-              </div>
-              <div className="col-md-4">
-                <label htmlFor="edad maxima">Edad maxima:</label>
-                <Field
-                  className="form-control"
-                  name="edad-max"
-                  placeholder="max"
-                  type="int"
-                />
-              </div>
-            </div>
-            <div className="col-md-4">
-              <label htmlFor="notas">Notas:</label>
-              <Field
-                className="form-control"
-                name="notas"
-                placeholder="especificaciones"
-                type="text-area"
-              />
-            </div>
-            <div className="row">
-              <div className="col-md-4">
-                <h2>fecha inicio</h2>
-                <DatePicker
-                  selected={startDate}
-                  onChange={(date) => setStartDate(date)}
-                />
-              </div>
-              <div className="col-md-4">
-                <h2>fecha final</h2>
+                    <div className="col-md-6">
+                      <label htmlFor="edad minima">Edad minima:</label>
+                      <Field
+                        className="form-control"
+                        name="edad-min"
+                        placeholder="min"
+                        type="int"
+                      />
+                    </div>
+                    <div className="col-md-6">
+                      <label htmlFor="edad maxima">Edad maxima:</label>
+                      <Field
+                        className="form-control"
+                        name="edad-max"
+                        placeholder="max"
+                        type="int"
+                      />
+                    </div>
+                    <div className="col-md-12">
+                      <label htmlFor="notas">Notas:</label>
+                      <Field
+                        className="form-control"
+                        name="notas"
+                        placeholder="especificaciones"
+                        component="textarea"
+                        rows=""
+                        cols="100"
+                      />
+                    </div>
+                    <div className="row">
+                      <div className="col-md-6">
+                        <label>Fecha Inicio:</label>
+                        <DatePicker
+                          selected={startDate}
+                          onChange={(date) => setStartDate(date)}
+                        />
+                      </div>
+                      <div className="col-md-6">
+                        <label>Fecha Final:</label>
 
-                <DatePicker
-                  selected={startDate}
-                  onChange={(date) => setStartDate(date)}
-                />
+                        <DatePicker
+                          selected={startDate}
+                          onChange={(date) => setStartDate(date)}
+                        />
+                      </div>
+                    </div>
+                    <div className="col-md-6 mt-4">
+                      <button className="btn btn-primary" type="submit">
+                        Enviar
+                      </button>
+                    </div>
+                    <div className="col-md-6 mt-4">
+                      <button className="btn btn-warning" type="reset">
+                        Cancelar
+                      </button>
+                    </div>
+                  </div>
+                </Form>
               </div>
-              <div className="col-md-4"></div>
             </div>
-            <button type="submit">offer</button>
-            <button type="reset">cancel</button>
-          </Form>
+          </div>
         )}
       </Formik>
     </div>
