ec692e6449784c11653eaee470cc26a7bf88fed2
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-11-01 22:41:53 -0300

    added SidebarUser component annd modified structure of View

--- src/views/Service.js
+++ src/views/Service.js
@@ -1,7 +1,19 @@
 import React from "react";
-import Services from "../components/Services"
+import Services from "../components/Services";
+import SidebarUser from "../components/SidebarUser";
 const Service = () => {
-  return <Services/>;
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-4">
+          <SidebarUser />
+        </div>
+        <div className="col-md-8">
+          <Services />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Service;
