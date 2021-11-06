9533ecc960ee5f417f82268d4d6dd1b491e2ad15
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-11-05 21:27:04 -0300

    updated file without a lot of changes

--- src/views/ServicesPublication.js
+++ src/views/ServicesPublication.js
@@ -1,45 +1,43 @@
-import React, { useState, useEffect, useContext  } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Context } from "../store/appContext";
 import PublicacionServicios from "../components/PublicacionServicios";
 import Swal from "sweetalert2";
 
 const ServicesPublication = () => {
-    const { store, actions } = useContext(Context);
-    const [services, setServices] = useState([]);
-    
-    const profil = JSON.parse(localStorage.getItem("loginUser"));
-    console.log(profil.user.id)
-    let id =profil.user.id;
-    // console.log(id);
-    useEffect(() => {
-      const REST_API_URL = `http://localhost:5000/service_publication/${id}`;
-      fetch(REST_API_URL, {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-      })
-        .then((response) => response.json())
-        .then((data) => setServices(data));
-    }, []);
+  const { store, actions } = useContext(Context);
+  const [services, setServices] = useState([]);
 
-    
+  const profil = JSON.parse(localStorage.getItem("loginUser"));
+  console.log(profil.user.id);
+  let id = profil.user.id;
+  // console.log(id);
+  useEffect(() => {
+    const REST_API_URL = `http://localhost:5000/service_publication/${id}`;
+    fetch(REST_API_URL, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then((data) => setServices(data));
+  }, []);
 
-    return (
-      <>
-        <div className="row">
-          <div className=" mt-4 pb-4 pt-2 mb-4">
-            {services.map((service, i) => (
-              <div className="col-md-12 my-3">
-                <PublicacionServicios
-                  key={i}
-                  data={service}
-                  image="https://via.placeholder.com/400x200"
-                />
-              </div>
-            ))}
-          </div>
+  return (
+    <>
+      <div className="row">
+        <div className=" mt-4 pb-4 pt-2 mb-4">
+          {services.map((service, i) => (
+            <div className="col-md-12 my-3">
+              <PublicacionServicios
+                key={i}
+                data={service}
+                image="https://via.placeholder.com/400x200"
+              />
+            </div>
+          ))}
         </div>
-      </>
-    );
-  };
+      </div>
+    </>
+  );
+};
 
 export default ServicesPublication;
