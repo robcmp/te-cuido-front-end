01bc02a169be0a184e5f5da446f062fe54abab47
Ruben Colmenares <ruben.colmenares@iesa.edu.ve>
2021-11-08 20:56:08 -0300

    Colocados condicionales en el sidebar para aparicion de rutas de acuerdo al rol. Existe problema de sintaxis para validar existencia de usuario autenticado

--- src/components/SidebarUser.js
+++ src/components/SidebarUser.js
@@ -1,23 +1,49 @@
-import React from "react";
+import React,{useContext} from "react";
+import { Context } from "../store/appContext";
+import { useHistory } from "react-router-dom";
 import "../styles/sidebarUser.css";
 import { slide as Menu } from "react-burger-menu";
 import { Link } from "react-router-dom";
 
-const SidebarUser = () => {
+const SidebarUser = (props) => {
+
+  const {store, action } = useContext(Context);
+
+  const getStorage = localStorage.getItem("loginUser")
+
+
+
   return (
     <Menu>
       <Link className="menu-item" to="/user/profile">
         <i className="bi bi-person-square"></i>Información Personal
       </Link>
+{ getStorage == undefined ? store.profileUser.user.role===2 ? 
       <Link className="menu-item" to="/user/service_publication">
         <i className="bi bi-cart-dash"></i>Publicación de Servicios
-      </Link>
+      </Link> : <></>
+      
+      : }
+{ store.profileUser.user.role===2 ?
       <Link className="menu-item" to="">
         <i className="bi bi-cart-dash"></i>Pendientes/Aceptados
-      </Link>
+      </Link> :  
       <Link className="menu-item" to="">
-        <i className="bi bi-cart-dash"></i>Lista de Servicios
+        <i className="bi bi-cart-dash"></i>Reserva Por Pagar/Agendados
       </Link>
+}
+
+{ store.profileUser.user.role===2 ?
+      <Link className="menu-item" to="">
+      <i className="bi bi-cart-dash"></i>Servicios Creados
+    </Link>
+      
+       :
+       <Link className="menu-item" to="">
+       <i className="bi bi-cart-dash"></i>Lista de Servicios
+     </Link>
+
+}
     </Menu>
   );
 };
