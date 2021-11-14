d6379a0e3e8a3c78b5a3f50c7477b40469c88e68
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-11-14 19:51:24 -0300

    modified routes to show and handle correctly the flow and navigation in the app

--- src/components/SidebarUser.js
+++ src/components/SidebarUser.js
@@ -1,4 +1,4 @@
-import React,{useContext,useEffect} from "react";
+import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 import { useHistory } from "react-router-dom";
 import "../styles/sidebarUser.css";
@@ -6,46 +6,46 @@ import { slide as Menu } from "react-burger-menu";
 import { Link } from "react-router-dom";
 
 const SidebarUser = (props) => {
-
-  const {store, actions} = useContext(Context);
+  const { store, actions } = useContext(Context);
   const history = useHistory();
-  
-  const getStorage = JSON.parse(localStorage.getItem("loginUser"));
-  console.log(getStorage);
 
-  
+  const getStorage = JSON.parse(localStorage.getItem("loginUser"));
 
   return (
     <Menu>
       <Link className="menu-item" to="/user/profile">
         <i className="bi bi-person-square"></i>Información Personal
       </Link>
-{ getStorage.user.role != undefined ? getStorage.user.role===2 ? 
-      <Link className="menu-item" to="/user/service_publication">
-        <i className="bi bi-cart-dash"></i>Publicación de Servicios
-      </Link> : <></>
-      
-      : history.push("/login")}
-{ getStorage.user.role===2 ?
-      <Link className="menu-item" to="">
-        <i className="bi bi-cart-dash"></i>Pendientes/Aceptados
-      </Link> :  
-      <Link className="menu-item" to="">
-        <i className="bi bi-cart-dash"></i>Reserva Por Pagar/Agendados
-      </Link>
-}
+      {getStorage.user.role !== undefined ? (
+        getStorage.user.role === 2 ? (
+          <Link className="menu-item" to="/user/service_publication">
+            <i className="bi bi-cart-dash"></i>Publicación de Servicios
+          </Link>
+        ) : (
+          <></>
+        )
+      ) : (
+        history.push("/login")
+      )}
+      {getStorage.user.role === 2 ? (
+        <Link className="menu-item" to="/user/service_confirmation">
+          <i className="bi bi-cart-dash"></i>Pendientes
+        </Link>
+      ) : (
+        <Link className="menu-item" to="/user/client_reservation">
+          <i className="bi bi-cart-dash"></i>Reserva Por Pagar/Agendados
+        </Link>
+      )}
 
-{ getStorage.user.role===2 ?
-      <Link className="menu-item" to="/user/services">
-      <i className="bi bi-cart-dash"></i>Servicios Creados
-    </Link>
-      
-       :
-       <Link className="menu-item" to="/user/list_publication">
-       <i className="bi bi-cart-dash"></i>Lista de Servicios
-     </Link>
-
-}
+      {getStorage.user.role === 2 ? (
+        <Link className="menu-item" to="/user/service_history">
+          <i className="bi bi-cart-dash"></i>Historial de Servicios
+        </Link>
+      ) : (
+        <Link className="menu-item" to="/user/list_publication">
+          <i className="bi bi-cart-dash"></i>Lista de Servicios
+        </Link>
+      )}
     </Menu>
   );
 };
