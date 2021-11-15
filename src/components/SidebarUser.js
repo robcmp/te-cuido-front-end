b6e14bf143257b3f1b67a1fb3b5da3753fbf71cd
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-11-14 23:59:09 -0300

    added logOut function, uncommented import for useHistory hook from reactRouterDom, refactorized code that handle the role links showing, added link to logOut the user

--- src/components/SidebarUser.js
+++ src/components/SidebarUser.js
@@ -1,19 +1,27 @@
 import React from "react";
 // import { Context } from "../store/appContext";
-// import { useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import "../styles/sidebarUser.css";
 import { slide as Menu } from "react-burger-menu";
 import { Link } from "react-router-dom";
 
 const SidebarUser = (props) => {
   // const { store, actions } = useContext(Context);
-  // const history = useHistory();
+  const history = useHistory();
 
   const getStorage = JSON.parse(localStorage.getItem("loginUser"));
 
+  const logOut = () => {
+    localStorage.clear();
+    history.push("/login");
+  };
+
   return (
     <Menu>
-      <Link className="menu-item" to="/user/profile">
+      <Link
+        className="menu-item"
+        to={getStorage.user.role === 2 ? "/carer/profile" : "/user/profile"}
+      >
         <i className="bi bi-person-square"></i>Información Personal
       </Link>
       {getStorage.user.role === 2 ? (
@@ -23,16 +31,19 @@ const SidebarUser = (props) => {
       ) : (
         <></>
       )}
-      {getStorage.user.role === 2 ? (
-        <Link className="menu-item" to="/carer/service_confirmation">
-          <i className="bi bi-cart-dash"></i>Pendientes
-        </Link>
-      ) : (
-        <Link className="menu-item" to="/user/client_reservation">
-          <i className="bi bi-cart-dash"></i>Reserva Por Pagar/Agendados
-        </Link>
-      )}
-
+      <Link
+        className="menu-item"
+        to={
+          getStorage.user.role === 2
+            ? "/carer/service_confirmation"
+            : "/user/client_reservation"
+        }
+      >
+        <i className="bi bi-cart-dash"></i>
+        {getStorage.user.role === 2
+          ? "Reservas Pendiente"
+          : "Reserva Por Pagar/Agendados"}
+      </Link>
       {getStorage.user.role === 2 ? (
         <Link className="menu-item" to="/carer/service_history">
           <i className="bi bi-cart-dash"></i>Historial de Servicios
@@ -42,6 +53,9 @@ const SidebarUser = (props) => {
           <i className="bi bi-cart-dash"></i>Lista de Servicios
         </Link>
       )}
+      <Link className="menu-item" to="/login" onClick={logOut}>
+        <i class="bi bi-box-arrow-left"></i>Cerrar Sesion
+      </Link>
     </Menu>
   );
 };
